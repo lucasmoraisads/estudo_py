70b880d193eb924ab505dfb24defb59e0951e440
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2025-03-03 13:46:03 -0300

    Continuação da aula

---
+++ fundamentos/8-metodosString.py
@@ -0,0 +1,13 @@
+movieName = "Esquadrão Classe A"
+descricaoFilme = """ Filme de Ação,
+com comedia e muitos tiros
+"""
+print(movieName.upper())
+print(movieName.lower())
+print(movieName.capitalize())
+print(movieName.title())
+print(movieName.center(10, "-"))
+print(movieName.find("a"))
+print(movieName.find("s"))
+print(movieName.replace("Classe", "classe"))
+print(descricaoFilme.split(","))
