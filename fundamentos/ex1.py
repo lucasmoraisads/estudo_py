1bf967b16c45da0be1fb8382f281f15fd14cf926
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2025-03-03 14:06:50 -0300

    Create ex1.py

---
+++ fundamentos/ex1.py
@@ -0,0 +1,17 @@
+# # Primeiro exercicio
+# nome = input("Digite seu nome: ")
+# sobreNome = input("Digite seu sobre nome: ")
+
+# nomeCompleto = (f"{nome} {sobreNome}")
+
+# print(nomeCompleto)
+
+# exercicio 2
+
+# texto = "Python e muito interresante para aprender"
+# palavra = texto.split()
+# textoInvertido = " ".join(palavra[::-1])
+# print(textoInvertido)
+
+# exercicio 3
+
